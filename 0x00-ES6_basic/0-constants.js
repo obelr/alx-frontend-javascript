891c017a9589830737f2645caebe83fdea30b2cb
obel <rdebebe@kifiya.et>
2022-12-19 17:24:57 +0300

    add Eslint-disable

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,7 @@
-export function taskFirst(){
-    const task = 'I prefer const when I can.';
-    return task;
+/* eslint-disable */
+export function taskFirst() {
+  const task = "I prefer const when I can.";
+  return task;
 }
 export function getLast() {
   return " is okay";
